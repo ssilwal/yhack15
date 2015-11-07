bd771612e4274218d8bb524d9e4d54cce4e8adf2
Sneha Silwal <ssilwal@users.noreply.github.com>
2015-11-07 17:15:37 -0500

    Need to add ASR & NLU to app.

---
+++ intern1/intern1/SKSConfiguration.h
@@ -0,0 +1,25 @@
+//
+//  SKSConfiguration.h
+//  intern1
+//
+//  SpeechKitSample
+//
+//  Copyright (c) 2015 Nuance Communications. All rights reserved.
+//
+
+#ifndef intern1_SKSConfiguration_h
+#define intern1_SKSConfiguration_h
+
+#import <Foundation/Foundation.h>
+
+extern NSString* SKSAppId;
+extern NSString* SKSServerHost;
+extern NSString* SKSServerPort;
+
+extern NSString* SKSNLUContextTag;
+
+extern NSString* SKSServerUrl;
+
+extern unsigned char SKSAppKey[];
+
+#endif
